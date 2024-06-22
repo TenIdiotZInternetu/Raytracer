37ec6ba5ceee0edc6690a2dc362ed0fa076b2755
TIZI <tenidiotzinternetudefault@gmail.com>
2024-06-22 17:24:04 +0200

    Implemented Ray.Transform for Matrix transformations.

--- Project/Optics/Ray.cs
+++ Project/Optics/Ray.cs
@@ -2,6 +2,7 @@ using OpenTK;
 using OpenTK.Mathematics;
 using rt004.SceneDefinition.Solids;
 using rt004.Utils;
+using Vector3 = OpenTK.Mathematics.Vector3;
 using Void = rt004.SceneDefinition.Solids.Void;
 
 namespace rt004.Optics;
@@ -30,5 +31,16 @@ public struct Ray
     public Vector3 At(float distanceT)
     {
         return Origin + distanceT * Direction;
+    }
+
+    public Ray Transform(Matrix4 transformation)
+    {
+        Vector4 homogeneousOrigin = Origin.ToHomogenous();
+        Vector4 homogeneousDirection = Direction.ToHomogenous();
+        
+        Vector3 newOrigin = (homogeneousOrigin * transformation).To3d();
+        Vector3 newDirection = ((homogeneousDirection * transformation).To3d() - newOrigin).Normalized();
+        
+        return new Ray(newOrigin, newDirection, ContainingSolid);
     }
 }
